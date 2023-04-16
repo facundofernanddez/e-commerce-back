ca5950ad8219631cd9b02eea158cf2d9c624cfab
Facundo Fernandez <ff96_@hotmail.com>
2023-04-16 11:05:38 -0300

    update regex email function

--- src/main/java/com/ecommerce/ecommerce/registration/EmailValidator.java
+++ src/main/java/com/ecommerce/ecommerce/registration/EmailValidator.java
@@ -2,14 +2,22 @@ package com.ecommerce.ecommerce.registration;
 
 import org.springframework.stereotype.Service;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
     
     @Override
-    public boolean test(String o) {
-        //TODO: 
-        //regex to validate email
-        return true;
+    public boolean test(String email) {
+        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+        "[a-zA-Z0-9_+&*-]+)*@" +
+        "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
     }
 }
